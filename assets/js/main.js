46a0a60bfc0ec4bf2ad93866af0478111f94ef5f
mastico ツ <89032409+mastiico@users.noreply.github.com>
2021-09-18 15:46:01 -0300

    Atualização não digitar LETRAS na calculadora

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+function SomenteNumero(e){
+    var tecla=(window.event)?event.keyCode:e.which;   
+    if((tecla>47 && tecla<58)) return true;
+    else{
+    	if (tecla==8 || tecla==0) return true;
+	else 
+     if ((tecla>8 && tecla<223)) return false;
+     return false
+    }
+}
+
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
